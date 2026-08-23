ed06543c73a99c4c7b409391f8a5301a70f1333e
linmeideli <2558527373@qq.com>
2026-08-23 20:03:58 +0800

    fix:HnS Crash

---
+++ src/Patches/HnsDisablePatch.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TONX.Patches;
+
+public class HnsDisablePatch
+{
+    [HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
+    public class MainMenuManagerStartPatch
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            SceneManager.add_sceneLoaded((Action<Scene, LoadSceneMode>)((scene, _) =>
+            {
+                if (!scene.name.Equals("MatchMaking", StringComparison.Ordinal)) return;
+                GameObject.Find("CreateHnSGameButton").SetActive(false);
+            }));
+        }
+    }
+    [HarmonyPatch(typeof(CreateGameOptions), nameof(CreateGameOptions.Show))]
+    static class CreateGameOptionsOpenShowPatch
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            GameObject.Find("HideSeekOption").SetActive(false);
+        }
+    }
+}
